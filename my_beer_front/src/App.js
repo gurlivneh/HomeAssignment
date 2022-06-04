ed9458fe9b6e804c76ff2b205b8ded8901edec53
unknown <gurlivneh@gmail.com>
2022-06-04 02:46:37 -0700

    search

--- my_beer_front/src/App.js
+++ my_beer_front/src/App.js
@@ -1,13 +1,3 @@
-import './App.css';
-import Main from './screens/Main';
-import Favorites from './screens/Favorites';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  Switch
-} from "react-router-dom";
 import MyRouter from './navigation/MyRouter';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
--- my_beer_front/src/screens/Main.js
+++ my_beer_front/src/screens/Main.js
@@ -9,36 +9,80 @@ import { searchBeersAndFood } from '../requests/Requests';
 const Main = () => {
     const beers = useSelector(state => state.beers)
     const dispatch = useDispatch()
-    console.log('beers', beers)
-    useEffect(() => {
-        if(beers.length === 0) {
-            searchBeersAndFood(1,'sushi').then(res => {
-                dispatch(beersSet(res)) 
+    const [foodInput, setFoodInput] = useState("")
+    const [pageNum, setPageNum] = useState(1)
 
-            })
-        }
-    }, []);
+    // console.log('beers', beers)
+    // useEffect(() => {
+    //     if (beers.length === 0) {
+    //         searchBeersAndFood(1, 'sushi').then(res => {
+    //             dispatch(beersSet(res))
+
+    //         })
+    //     }
+    // }, []);
+
+    const handleChangeEvent = (e)  => {
+        setFoodInput(e.target.value);
+      }
+      const handleSearchClick = ()  => {
+        searchBeersAndFood(pageNum, foodInput).then((res) => {
+            dispatch(beersSet(res))
+        })      
+    }
 
     return (
-        <Container>
-            {beers && beers.map( (item) => {
-                return(
-                    <BeerCard item={item} key={item.id}/>
-                )
-            })}
-        </Container>
+        <MainContainer>
+            Food Pairing:  
+            <Input onChange={handleChangeEvent}/>
+            <SearchButton onClick={handleSearchClick}>Search</SearchButton> 
+            <BeersContainer>
+                {beers && beers.map((item) => {
+                    return (
+                        <BeerCard item={item} key={item.id} />
+                    )
+                })}
+            </BeersContainer>
+        </MainContainer>
     )
 }
 
 export default Main
 
+const MainContainer = styled.div`
+    background-color: green;
+    padding: 20px;
+    justify-content: center;
+    align-items: center;
+
+`;
 
 
-const Container = styled.div`
+const BeersContainer = styled.div`
     display: flex;
     flex-wrap:wrap;
     justify-content: space-evenly;
     align-items: center;
     background-color: gray;
+`;
+
+const Input = styled.input`
+    align-items: center;
+    justify-content: center;
+    /* background-color: blue; */
+    height: 30px;
+    width: 150px;
+    margin: 10px;
+
+   
+`;
+
+const SearchButton = styled.button`
+    align-items: center;
+    justify-content: center;
+    /* background-color: blueviolet; */
+    height: 25px;
+    width: 100px;
+    margin: 10px;
 `;
 
